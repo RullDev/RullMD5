29e25f1dbf2bb7ed449fed686562b552a1ec2461
RullDev <105213064+RullDev@users.noreply.github.com>
2022-10-14 13:32:45 +0700

    Update info-update.js

--- plugins/info-update.js
+++ plugins/info-update.js
@@ -7,7 +7,7 @@ let text = `
 ❏────
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: 'Lainya', url: 'https://fangzbot.websites.co.in/'}},
+    {index: 1, urlButton: {displayText: 'Lainya', url: 'https://bit.ly/3eggcxd'}},
 ]
 let tm = {
 text: text,
